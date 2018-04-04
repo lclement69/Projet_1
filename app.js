caec6279d5e35b4f94059ef078727e65e616cb3d
Lionel CLEMENT <lclement@orange.fr>
2018-04-04 17:48:23 +0200

    reste la carte plus finition

---
+++ app.js
@@ -0,0 +1,12 @@
+// AIzaSyAGEvnWhSvv_KGP41ifsz6JtQ1yD9Leg98 
+var map; 
+function initMap() { 
+	map = new google.maps.Map(document.getElementById('map'), 
+		{ 
+			center: {
+				lat: -34.397, 
+				lng: 150.644
+			}, 
+			zoom: 8 
+		}); 
+}
