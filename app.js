110202a0fc95826d74244bc6344ad788b9f7f5b5
Lionel CLEMENT <lclement@orange.fr>
2018-04-16 22:07:11 +0200

    carte enfin mise

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 // AIzaSyAGEvnWhSvv_KGP41ifsz6JtQ1yD9Leg98 
-var map; 
-function initMap() { 
-	map = new google.maps.Map(document.getElementById('map'), 
-		{ 
-			center: {
-				lat: -34.397, 
-				lng: 150.644
-			}, 
-			zoom: 8 
-		}); 
+var map;
+function initMap() {
+  var myLatLng = {lat: 48.873162, lng: 2.349839};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: myLatLng
+  });
+
+   var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+  });
 }
+
+var ulMenu = document.querySelector('[id^=drop]:checked + ul'); 
+ulMenu.addEventListener('click', function() { 
+	ulMenu.style.display = 'none';
+});
